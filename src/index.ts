fc10d6c08b937124b4b037675a4f4ad740ff7c33
Christian Betz <christian.betz@gmail.com>
2020-02-21 23:15:05 -0500

    working client example!

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,145 @@ import clear from 'clear';
 import figlet from 'figlet';
 import program from 'commander';
 
-clear();
+import RSocketWebSocketClient from 'rsocket-websocket-client';
+import {RSocketClient, MAX_STREAM_ID} from 'rsocket-core';
+import {Responder, Payload, ReactiveSocket, ISubscription}  from 'rsocket-types';
+import {Flowable, Single}  from 'rsocket-flowable';
+import Deferred from 'fbjs/lib/Deferred'; // TODO: needs typings (we cannot use strict because of this)
+import WsWebSocket = require('ws'); // TODO: Requires hack to RsocketJS typings. This is because a WsWebSocket is not the same as standard WebSocket
+
 console.log(
     chalk.red(
-        figlet.textSync('pizza-cli', { horizontalLayout: 'full' })
+        figlet.textSync('rsocket-typescript', { horizontalLayout: 'full' })
     )
 );
 
 program
     .version('0.0.1')
-    .description('An example CLI for ordering pizzas')
-    .option('-p, --peppers', "Add peppers")
-    .option('-a, --anchovies', "Add anchovies")
-    .option('-c, --cheese <type>', "Add cheese of type", "mozzarella")
-    .option('-C, --no-cheese', "You do not want any cheese")
+    .description('An rsocket typescript example')
+    .option('-h, --host <localhost>', "Host", "localhost")
+    .option('-p, --port <8181>', "Port", "8181")
     .parse(process.argv);
 
-console.log('you ordered a pizza with:');
+var logRequest = console.log;
 
-if (program.peppers) console.log(' - peppers');
-if (program.anchovies) console.log(' - anchovies');
+function make(data: string): Payload<string, string> {
+    return {
+        data,
+        metadata: ''
+    }
+}
 
-const cheese: string = true === program.cheese ? 'mozzarella' : program.cheese || 'no';
+class SymmetricResponder implements Partial<Responder<string, string>> {
+    fireAndForget(payload: Payload<string,string>): void {
+        logRequest('fnf', payload);
+    }
 
-console.log(' - %s cheese', cheese);
+    requestResponse(payload: Payload<string, string>): Single<Payload<string, string>> {
+        logRequest('requestResponse', payload);
+        return Single.error(new Error());
+    }
 
+    requestStream(payload: Payload<string, string>): Flowable<Payload<string, string>> {
+        logRequest('requestStream', payload);
+        return Flowable.just(make('Hello '), make('world!'));
+    }
+
+    requestChannel(payloads: Flowable<Payload<string, string>>): Flowable<Payload<string, string>> {
+        return Flowable.error(new Error());
+    }
+
+    metadataPush(payload: Payload<string, string>): Single<void> {
+        logRequest('metadataPush', payload);
+        return Single.error(new Error()); // TODO: is the typing for Single.error() wrong?
+    }
+}
+
+function doOperation(
+  socket: ReactiveSocket<string, string>,
+  operation: string,
+  payload: string,
+): Flowable<Payload<string, string>> {
+  switch (operation) {
+    case 'none':
+      return Flowable.never();
+    case 'stream':
+    default:
+      console.log(`Requesting stream with payload: ${payload}`);
+      return socket.requestStream({
+        data: payload,
+        metadata: '',
+      });
+  }
+}
+
+interface OperationOptions {
+    operation: string;
+    payload: string;
+}
+
+function runOperation(socket: ReactiveSocket<string, string>, options: OperationOptions) {
+    const deferred = new Deferred();
+    let subscription: ISubscription;
+    doOperation(socket, options.operation, options.payload).subscribe({
+        onComplete() {
+            console.log('onComplete()');
+            deferred.resolve();
+        },
+        onError(error) {
+            console.log('onError(%s)', error.message);
+            deferred.reject(error);
+        },
+        onNext(payload) {
+            console.log('onNext(%s)', payload.data);
+        },
+        onSubscribe(_subscription) {
+            subscription = _subscription;
+            subscription.request(MAX_STREAM_ID);
+        },
+    });
+    return deferred.getPromise();
+}
+
+function connect() {
+    const client = new RSocketClient<string, string>({
+        setup: {
+            dataMimeType: 'text/plain',
+            keepAlive: 1000000, // TODO ADJUST thIS AFTER TEST
+            lifetime: 100000,
+            metadataMimeType: 'text/plain'
+        },
+        responder: new SymmetricResponder(),
+        transport: new RSocketWebSocketClient({
+            url: "ws://localhost:8181",
+            wsCreator: url => {
+                return new WsWebSocket(url);
+            }
+        })
+    });
+
+    return client.connect();
+}
+
+async function run(options) {
+    console.log(`Client connecting to ${options.host}:${options.port}`);
+    const socket: ReactiveSocket<string, string> = await connect();
+    socket.connectionStatus().subscribe(status => {
+        console.log("Connection status:", status);
+    });
+
+    return runOperation(socket, options);
+}
+
+//var w = new WebSocket("ws://localhost:8341");
+
+Promise.resolve(run(program)).then(
+    () => {
+        console.log('exit');
+        process.exit(0);
+    },
+    error => {
+        console.error(error.stack);
+        process.exit(1);
+    }
+);
